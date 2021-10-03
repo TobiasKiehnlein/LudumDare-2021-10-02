c54473ee2e518216a2694bbe2bfa57d9fa260973
Wartronic <axander971@gmail.com>
2021-10-03 11:24:37 +0200

    Gravity Slerp

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -14,14 +14,12 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private GameSettings gameSettings;
 
     // public GravityManager gravity;
-    
+
 
     private float _horizontalForce;
 
-    private bool _isUnAligned = true;
     private bool _jumpAllowed;
     private float _jumpLock;
-    private Orientation _oldOrientation;
     private Rigidbody2D _rg;
     private Vector2 _vel = new Vector2(0, 0);
 
@@ -34,41 +32,8 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // _rg.gravityScale = 1;
-        // // _currentOrientation = gravity.GETCurrentOrientation();
-        // _currentOrientation = Orientation.Down;
-        // var grav = _currentOrientation switch
-        // {
-        //     Orientation.Up => Vector2.up,
-        //     Orientation.Down => Vector2.down,
-        //     Orientation.Left => Vector2.left,
-        //     Orientation.Right => Vector2.right,
-        //     _ => Vector2.zero
-        // };
-        // grav *= 5;
-        //
-        // Physics2D.gravity = grav;
-        this.gameObject.transform.rotation = gameSettings.GravityOrientation switch
-        {
-            Orientation.Down => Quaternion.Euler(0, 0, 0),
-            Orientation.Up => Quaternion.Euler(0, 0, 180f),
-            Orientation.Left => Quaternion.Euler(0, 0, 270f),
-            Orientation.Right => Quaternion.Euler(0, 0, 90f),
-            _ => this.gameObject.transform.rotation
-        };
-/*   switch (_currentOrientation)
- {
-      case Orientation.Up:
-      case Orientation.Down:
-          isGrounded = (Abs(_rg.velocity.y) < 0.05f);
-          break;
-      case Orientation.Left:
-      case Orientation.Right:
-          isGrounded = (Abs(_rg.velocity.x) < 0.05f);
-          break;
-      default: break;
-  }*/
-
+       
+      
         _jumpLock -= Time.deltaTime;
         if (_jumpLock < 0)
         {
@@ -94,7 +59,8 @@ public class CharacterController : MonoBehaviour
 
             var currentVelocity = _rg.velocity;
 
-            if (gameSettings.GravityOrientation == Orientation.Down || gameSettings.GravityOrientation == Orientation.Up)
+            if (gameSettings.GravityOrientation == Orientation.Down ||
+                gameSettings.GravityOrientation == Orientation.Up)
             {
                 if (Mathf.Abs(_rg.velocity.x) > 5 && currentVelocity.x * _horizontalForce > 0) _horizontalForce = 0;
             }
@@ -127,6 +93,9 @@ public class CharacterController : MonoBehaviour
                     case Orientation.Right:
                         _rg.AddForce(new Vector2(-(jumpForce * _rg.mass), 0));
                         break;
+                    default:
+                        _rg.AddForce(new Vector2(0, jumpForce * _rg.mass));
+                        break;
                 }
 
 
@@ -134,27 +103,20 @@ public class CharacterController : MonoBehaviour
                 _jumpAllowed = false;
             }
 
-            switch (gameSettings.GravityOrientation)
+            _vel = gameSettings.GravityOrientation switch
             {
-                case Orientation.Up:
-                    _vel = new Vector2(-_horizontalForce, _rg.velocity.y);
-                    break;
-                case Orientation.Down:
-                    _vel = new Vector2(_horizontalForce, _rg.velocity.y);
-                    break;
-                case Orientation.Left:
-                    _vel = new Vector2(_rg.velocity.x, _horizontalForce);
-                    break;
-                case Orientation.Right:
-                    _vel = new Vector2(_rg.velocity.x, -_horizontalForce);
-                    break;
-            }
+                Orientation.Up => new Vector2(-_horizontalForce, _rg.velocity.y),
+                Orientation.Down => new Vector2(_horizontalForce, _rg.velocity.y),
+                Orientation.Left => new Vector2(_rg.velocity.x, _horizontalForce),
+                Orientation.Right => new Vector2(_rg.velocity.x, -_horizontalForce),
+                _ => new Vector2(_horizontalForce, _rg.velocity.y)
+            };
 
             _rg.velocity = _vel;
         }
 
 
-        //  if (_isUnAligned) AlignPlayer();
+        AlignPlayer(false);
 
         isGrounded = false;
     }
@@ -180,27 +142,32 @@ public class CharacterController : MonoBehaviour
         return input < 0f ? -input : input;
     }
 
-  
-    private void AlignPlayer()
-    {
-        float targetAngle = 0;
-        switch (gameSettings.GravityOrientation)
-        {
-            case Orientation.Up:
-                targetAngle = 180;
-                break;
-            case Orientation.Down:
-                targetAngle = 0;
-                break;
-            case Orientation.Left:
-                targetAngle = 90;
-                break;
-            case Orientation.Right:
-                targetAngle = 270;
-                break;
-        }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle),
-            turnSpeed * Time.deltaTime);
+    private void AlignPlayer(bool fallbackModeUsed)
+    {
+        if(fallbackModeUsed)
+        {
+            transform.rotation = gameSettings.GravityOrientation switch
+            {
+                Orientation.Down => Quaternion.Euler(0, 0, 0),
+                Orientation.Up => Quaternion.Euler(0, 0, 180f),
+                Orientation.Left => Quaternion.Euler(0, 0, 270f),
+                Orientation.Right => Quaternion.Euler(0, 0, 90f),
+                _ => transform.rotation
+            };
+        }else
+        {
+            float targetAngle = gameSettings.GravityOrientation switch
+            {
+                Orientation.Up => 180,
+                Orientation.Down => 0,
+                Orientation.Left => 90,
+                Orientation.Right => 270,
+                _ => 0
+            };
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle),
+                turnSpeed * Time.deltaTime);
+        }
     }
 }
