49f6feda5a8f0711da9a5ad495fd9e452dd28097
Tobias Kiehnlein <tobias@kiehnlein.com>
2021-10-04 00:11:01 +0200

    Make MiniMap transparent

--- Assets/Scripts/ToggleVisibility.cs
+++ Assets/Scripts/ToggleVisibility.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class ToggleVisibility : MonoBehaviour
 {
     [SerializeField] private KeyCode keyCode;
+    [SerializeField] private bool defaultValue = true;
     private Canvas _canvas;
 
     private void Start()
     {
         _canvas = GetComponent<Canvas>();
+        _canvas.enabled = defaultValue;
     }
 
     void Update()
